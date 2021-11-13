8c788fe257f3a2f03645420a74a869e6e3d786c8
“github’s <eason.zhai97@outlook.com>
2021-11-13 11:20:02 +0100

    logistic regression

--- pytorch basics & linear regression/logistic regression.py
+++ pytorch basics & linear regression/logistic regression.py
@@ -2,6 +2,11 @@ import torch
 import torchvision
 from torchvision.datasets import MNIST
 import matplotlib.pyplot as plt
+from torch.utils.data import random_split
+from torch.utils.data import DataLoader
+import torch.nn as nn
+import torchvision.transforms as transforms
+import torch.nn.functional as F
 
 # Download the dateset
 dataset = MNIST(root='data/', download=True)
@@ -19,35 +24,164 @@ print('Label:', label)
 
 # Transform the image data into tensor
 # The image is converted to 1x28x28 tensor
-import torchvision.transforms as transforms
+
 dataset = MNIST(root='data/',
                 train=True,
                 transform=transforms.ToTensor())
 
+print(len(dataset))
 # split the dataset into train and validation dataset
-
-from torch.utils.data import random_split
-train_ds, val_ds=random_split(dataset,[5000,1000])
-
+train_ds, val_ds=random_split(dataset, [50000, 10000])
+print(len(train_ds),len(val_ds))
 # load the data in baches
-from torch.utils.data import DataLoader
 batch_size = 128
 train_loader = DataLoader(train_ds, batch_size, shuffle=True)
 val_loader = DataLoader(val_ds, batch_size)
 
 # Define the training model
 # the logistic regression model is similar to linear regression model
-import torch.nn as nn
+
 input_size=28*28
 num_classes=10
 
 # Logistic regression model
 model=nn.Linear(input_size,num_classes)
 
+print(model.weight.shape)
 # reshape the image to a vector
 
+class MnistModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.linear = nn.Linear(input_size, num_classes)
+
+    def forward(self, xb):
+        xb = xb.reshape(-1, 784)
+        out = self.linear(xb)
+        return out
 
 
+model = MnistModel()
+
+for images, labels in train_loader:
+    print(images.shape)
+    outputs = model(images)
+    break
+
+print('outputs.shape : ', outputs.shape)
+print('Sample outputs :\n', outputs[:2].data)
+
+# Apply softmax for each output row
+probs = F.softmax(outputs, dim=1)
+
+# Look at sample probabilities
+print("Sample probabilities:\n", probs[:2].data)
+
+# Add up the probabilities of an output row
+print("Sum: ", torch.sum(probs[0]).item())
+max_probs, preds = torch.max(probs, dim=1)
+print(preds)
+print(max_probs)
+
+def accuracy(outputs, labels):
+    _, preds = torch.max(outputs, dim=1)
+    return torch.tensor(torch.sum(preds == labels).item() / len(preds))
+
+print(accuracy(outputs, labels))
+
+# define the loss function
+loss_fn = F.cross_entropy
 
 
+def fit(epochs, lr, model, train_loader, val_loader, opt_func=torch.optim.SGD):
+    optimizer = opt_func(model.parameters(), lr)
+    history = []  # for recording epoch-wise results
 
+    for epoch in range(epochs):
+
+        # Training Phase
+        for batch in train_loader:
+            loss = model.training_step(batch)
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+
+        # Validation phase
+        result = evaluate(model, val_loader)
+        model.epoch_end(epoch, result)
+        history.append(result)
+
+    return history
+
+def evaluate(model, val_loader):
+    outputs = [model.validation_step(batch) for batch in val_loader]
+    return model.validation_epoch_end(outputs)
+
+
+class MnistModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.linear = nn.Linear(input_size, num_classes)
+
+    def forward(self, xb):
+        xb = xb.reshape(-1, 784)
+        out = self.linear(xb)
+        return out
+
+    def training_step(self, batch):
+        images, labels = batch
+        out = self(images)  # Generate predictions
+        loss = F.cross_entropy(out, labels)  # Calculate loss
+        return loss
+
+    def validation_step(self, batch):
+        images, labels = batch
+        out = self(images)  # Generate predictions
+        loss = F.cross_entropy(out, labels)  # Calculate loss
+        acc = accuracy(out, labels)  # Calculate accuracy
+        return {'val_loss': loss, 'val_acc': acc}
+
+    def validation_epoch_end(self, outputs):
+        batch_losses = [x['val_loss'] for x in outputs]
+        epoch_loss = torch.stack(batch_losses).mean()  # Combine losses
+        batch_accs = [x['val_acc'] for x in outputs]
+        epoch_acc = torch.stack(batch_accs).mean()  # Combine accuracies
+        return {'val_loss': epoch_loss.item(), 'val_acc': epoch_acc.item()}
+
+    def epoch_end(self, epoch, result):
+        print("Epoch [{}], val_loss: {:.4f}, val_acc: {:.4f}".format(epoch, result['val_loss'], result['val_acc']))
+
+
+model = MnistModel()
+result0 = evaluate(model, val_loader)
+history1 = fit(5, 0.001, model, train_loader, val_loader)
+history2 = fit(5, 0.001, model, train_loader, val_loader)
+history3 = fit(5, 0.001, model, train_loader, val_loader)
+history4 = fit(5, 0.001, model, train_loader, val_loader)
+history = [result0] + history1 + history2 + history3 + history4
+accuracies = [result['val_acc'] for result in history]
+plt.plot(accuracies, '-x')
+plt.xlabel('epoch')
+plt.ylabel('accuracy')
+plt.title('Accuracy vs. No. of epochs')
+
+# test with individual images
+test_dataset = MNIST(root='data/',
+                     train=False,
+                     transform=transforms.ToTensor())
+
+img, label = test_dataset[0]
+plt.imshow(img[0], cmap='gray')
+print('Shape:', img.shape)
+print('Label:', label)
+
+
+def predict_image(img, model):
+    xb = img.unsqueeze(0)
+    yb = model(xb)
+    _, preds = torch.max(yb, dim=1)
+    return preds[0].item()
+
+img, label = test_dataset[0]
+plt.imshow(img[0], cmap='gray')
+print('Label:', label, ', Predicted:', predict_image(img, model))
