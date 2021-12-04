124b4d4b81a161a5cee1e1f85dc16944b3ad052d
“github’s <eason.zhai97@outlook.com>
2021-12-04 21:38:21 +0100

    learn pytorch

---
+++ pytorch basics & linear regression/tests.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+i=np.random.permutation(100)
+
+print(i)
--- pytorch basics & linear regression/train_dl_on_GPU.py
+++ pytorch basics & linear regression/train_dl_on_GPU.py
@@ -24,6 +24,10 @@ plt.imshow(image.permute(1, 2, 0), cmap='gray')
 plt.show()
 print('Label:', label)
 
+def split_indices(n, val_pct):
+    n_val=int(val_pct*n)
+    idxs=np.random.permutation(n)
+    return idxs[n_val:], idxs[:n_val]
 # split the data into train and validation
 val_size=10000
 train_size=len(dataset)-val_size
@@ -43,9 +47,166 @@ for images, _ in train_loader:
     break
 plt.show()
 
+for images, labels in train_loader:
+    print('images.shape:', images.shape)
+    inputs = images.reshape(-1, 784)
+    print('inputs.shape:', inputs.shape)
+    break
+input_size = inputs.shape[-1]
+hidden_size = 32
+layer1 = nn.Linear(input_size, hidden_size)
+layer1_outputs = layer1(inputs)
+print('layer1_outputs.shape:', layer1_outputs.shape)
+class MnistModel(nn.Module):
+    """Feedfoward neural network with 1 hidden layer"""
+
+    def __init__(self, in_size, hidden_size, out_size):
+        super().__init__()
+        # hidden layer
+        self.linear1 = nn.Linear(in_size, hidden_size)
+        # output layer
+        self.linear2 = nn.Linear(hidden_size, out_size)
+
+    def forward(self, xb):
+        # Flatten the image tensors
+        xb = xb.view(xb.size(0), -1)
+        # Get intermediate outputs using hidden layer
+        out = self.linear1(xb)
+        # Apply activation function
+        out = F.relu(out)
+        # Get predictions using output layer
+        out = self.linear2(out)
+        return out
+
+    def training_step(self, batch):
+        images, labels = batch
+        out = self(images)  # Generate predictions
+        loss = F.cross_entropy(out, labels)  # Calculate loss
+        return loss
+
+    def validation_step(self, batch):
+        images, labels = batch
+        out = self(images)  # Generate predictions
+        loss = F.cross_entropy(out, labels)  # Calculate loss
+        acc = accuracy(out, labels)  # Calculate accuracy
+        return {'val_loss': loss, 'val_acc': acc}
+
+    def validation_epoch_end(self, outputs):
+        batch_losses = [x['val_loss'] for x in outputs]
+        epoch_loss = torch.stack(batch_losses).mean()  # Combine losses
+        batch_accs = [x['val_acc'] for x in outputs]
+        epoch_acc = torch.stack(batch_accs).mean()  # Combine accuracies
+        return {'val_loss': epoch_loss.item(), 'val_acc': epoch_acc.item()}
+
+    def epoch_end(self, epoch, result):
+        print("Epoch [{}], val_loss: {:.4f}, val_acc: {:.4f}".format(epoch, result['val_loss'], result['val_acc']))
+def accuracy(outputs, labels):
+    _, preds = torch.max(outputs, dim=1)
+    return torch.tensor(torch.sum(preds == labels).item() / len(preds))
+
+print(torch.cuda.is_available())
+
+def get_default_device():
+    """Pick GPU if available, else CPU"""
+    if torch.cuda.is_available():
+        return torch.device('cuda')
+    else:
+        return torch.device('cpu')
+
+input_size = 784
+hidden_size = 32 # you can change this
+num_classes = 10
+model = MnistModel(input_size, hidden_size=32, out_size=num_classes)
+for t in model.parameters():
+    print(t.shape)
+device = get_default_device()
+print(device)
+for images, labels in train_loader:
+    outputs = model(images)
+    loss = F.cross_entropy(outputs, labels)
+    print('Loss:', loss.item())
+    break
+
+print('outputs.shape : ', outputs.shape)
+print('Sample outputs :\n', outputs[:2].data)
+
+def to_device(data, device):
+    """Move tensor(s) to chosen device"""
+    if isinstance(data, (list,tuple)):
+        return [to_device(x, device) for x in data]
+    return data.to(device, non_blocking=True)
+
+for images, labels in train_loader:
+    print(images.shape)
+    images = to_device(images, device)
+    print(images.device)
+    break
 
 
+class DeviceDataLoader():
+    """Wrap a dataloader to move data to a device"""
 
+    def __init__(self, dl, device):
+        self.dl = dl
+        self.device = device
 
+    def __iter__(self):
+        """Yield a batch of data after moving it to device"""
+        for b in self.dl:
+            yield to_device(b, self.device)
 
+    def __len__(self):
+        """Number of batches"""
+        return len(self.dl)
 
+train_loader = DeviceDataLoader(train_loader, device)
+val_loader = DeviceDataLoader(val_loader, device)
+
+def evaluate(model, val_loader):
+    """Evaluate the model's performance on the validation set"""
+    outputs = [model.validation_step(batch) for batch in val_loader]
+    return model.validation_epoch_end(outputs)
+
+def fit(epochs, lr, model, train_loader, val_loader, opt_func=torch.optim.SGD):
+    """Train the model using gradient descent"""
+    history = []
+    optimizer = opt_func(model.parameters(), lr)
+    for epoch in range(epochs):
+        # Training Phase
+        for batch in train_loader:
+            loss = model.training_step(batch)
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+        # Validation phase
+        result = evaluate(model, val_loader)
+        model.epoch_end(epoch, result)
+        history.append(result)
+    return history
+
+model = MnistModel(input_size, hidden_size=hidden_size, out_size=num_classes)
+to_device(model, device)
+history = [evaluate(model, val_loader)]
+history += fit(5, 0.5, model, train_loader, val_loader)
+history += fit(5, 0.1, model, train_loader, val_loader)
+losses = [x['val_loss'] for x in history]
+figure=plt.figure()
+plt.plot(losses, '-x')
+plt.xlabel('epoch')
+plt.ylabel('loss')
+plt.title('Loss vs. No. of epochs')
+
+# Define test dataset
+test_dataset = MNIST(root='data/',
+                     train=False,
+                     transform=ToTensor())
+
+def predict_image(img, model):
+    xb = to_device(img.unsqueeze(0), device)
+    yb = model(xb)
+    _, preds  = torch.max(yb, dim=1)
+    return preds[0].item()
+
+img, label = test_dataset[0]
+plt.imshow(img[0], cmap='gray')
+print('Label:', label, ', Predicted:', predict_image(img, model))
